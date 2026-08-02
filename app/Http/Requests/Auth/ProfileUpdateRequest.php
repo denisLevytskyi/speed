6761364c2e614a3209fb9f836a667a462e0b0455
STORM <er9p21873@gmail.com>
2026-08-02 23:36:48 +0300

    RENAME PROFILEEDIT

--- app/Http/Requests/Auth/ProfileUpdateRequest.php
+++ app/Http/Requests/Auth/ProfileUpdateRequest.php
@@ -16,8 +16,8 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'profileEditName' => ['required','string', 'max:255'],
-            'profileEditEmail' => ['required', 'email', 'max:255', Rule::unique(User::class, 'email')->ignore($this->user()->id)],
+            'profileName' => ['required','string', 'max:255'],
+            'profileEmail' => ['required', 'email', 'max:255', Rule::unique(User::class, 'email')->ignore($this->user()->id)],
         ];
     }
 }
